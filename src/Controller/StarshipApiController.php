0bd28570935d92527e7df840a09fe4fd770aa150
DSebas09 <sebas250902@hotmail.com>
2025-08-14 22:53:01 -0600

    learning to use services and understand autowiring

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\model\Starship;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +11,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollections(): Response
+    public function getCollections(LoggerInterface $logger): Response
     {
+        $logger->info('Starships collection retrieved');
         $starships = [
             new Starship(
                 1,
