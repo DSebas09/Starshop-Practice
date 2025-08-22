4c8df3cb91bfae94390ce7bbe52b73384eba0ab2
DSebas09 <sebas250902@hotmail.com>
2025-08-22 00:39:21 -0600

    A new endpoint is created and existing ones are improved

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -9,13 +9,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships')]
+    #[Route('', methods: ['GET'])]
     public function getCollections(StarshipRepository $starshipRepository): Response
     {
         $starships = $starshipRepository->findAll();
 
         return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', methods: ['GET'])]
+    public function get(int $id, StarshipRepository $starshipRepository): Response
+    {
+        $starship = $starshipRepository->findById($id);
+
+        if (!$starship) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+
+        return $this->json($starship);
     }
 }
