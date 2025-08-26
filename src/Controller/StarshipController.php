66fc5b70d9d573f379f4f5a9b632c3b27c19a4f2
DSebas09 <sebas250902@hotmail.com>
2025-08-26 00:27:57 -0600

    learning to generate URLs

---
+++ src/Controller/StarshipController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StarshipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class StarshipController extends AbstractController
+{
+    #[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
+    public function show(int $id, StarshipRepository $starshipRepository): Response
+    {
+        $ship = $starshipRepository->findById($id);
+
+        if (!$ship) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+
+        return $this->render('starship/show.html.twig', [
+            'ship' => $ship,
+        ]);
+    }
+}
